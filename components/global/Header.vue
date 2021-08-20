78e6ca953e78b7482872da09700973379f00a9ed
Corentin <corentin7301@gmail.com>
2021-08-20 12:01:55 +0200

    header

---
+++ components/global/Header.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="py-10">
+        <img src="@/assets/img/logo.png" alt="wildcount" class=" mx-auto h-[10vh]">
+        <p class=" absolute top-0 right-0">?</p>
+    </div>
+</template>
