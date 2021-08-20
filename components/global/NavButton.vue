9a8e8b1525f806de9b8ddc6a2c6bd21ed03eb012
Corentin <corentin7301@gmail.com>
2021-08-20 12:02:25 +0200

    navbar

---
+++ components/global/NavButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <nuxt-link :to="url" class=" flex justify-center items-center rounded-full h-14 w-14 bg-backgroundColor">
+    <slot />
+  </nuxt-link>
+
+</template>
+
+<script>
+  export default {
+    name: 'NavButton',
+    props: {
+      url: {
+        type: String,
+        default: ''
+      }
+    }
+  }
+
+</script>
