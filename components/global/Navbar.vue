f493c50876c2d2e848459e5dcf632b7ad39eb964
Corentin <corentin7301@gmail.com>
2021-08-27 15:03:00 +0200

    add active style for all routes who are '/'

--- components/global/Navbar.vue
+++ components/global/Navbar.vue
@@ -1,20 +1,20 @@
 <template>
   <div class=" bg-darkGrey h-[85px] w-full fixed bottom-0 flex justify-around items-center">
-    <nav-button url="/">
-    <p class=" hidden">Page d'accueil</p>
+    <nav-button url="/" :class="{'active' : isRouteActive}">
+      <p class=" hidden">Page d'accueil</p>
       <svg class="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
       </svg>
     </nav-button>
     <nav-button url="/list">
-    <p class=" hidden">Page de liste des observations</p>
+      <p class=" hidden">Page de liste des observations</p>
       <svg class="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 10h16M4 14h16M4 18h16">
         </path>
       </svg>
     </nav-button>
     <nav-button url="/settings">
-    <p class=" hidden">Page de paramètres</p>
+      <p class=" hidden">Page de paramètres</p>
       <svg class="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
           d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z">
@@ -25,3 +25,19 @@
     </nav-button>
   </div>
 </template>
+
+<script>
+  export default {
+    computed: {
+      isRouteActive: function() {
+        if (this.$nuxt.$route.path == '/') {
+          return true
+        } else {
+          return false
+        }
+      }
+    },
+  }
+
+</script>
+
