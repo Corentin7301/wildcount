b97949ef1f727a3412f2501a2190c3950097b968
Corentin PERROUX <corentin7301@gmail.com>
2022-08-01 12:13:12 +0200

    add page & layout folder logic

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class=" p-10">
+        <slot />
+    </div>
+</template>
+
+<script setup>
+useHead({
+  title: 'My App',
+  // or, instead:
+  // titleTemplate: (title) => `My App - ${title}`,
+  viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+  charset: 'utf-8',
+  meta: [
+    { name: 'description', content: 'My amazing site.' }
+  ],
+  bodyAttrs: {
+    class: 'test'
+  }
+})
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1 class=" text-3xl">{{title}}</h1>
+  </div>
+
+</template>
+
+<script setup>
+  definePageMeta({
+    title: 'My home page'
+  })
+
+  const title = ref('My title')
+
+</script>
