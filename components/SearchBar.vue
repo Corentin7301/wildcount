a0ccd38cf2010daa4b18a4ccd274a860607b3f05
Corentin PERROUX <corentin7301@gmail.com>
2022-08-04 12:11:47 +0200

    add dropdown

---
+++ components/SearchBar.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="mt-1 relative rounded-md shadow-sm">
+    <input type="text" name="search" id="search" class="input-style text-2xl p-0 pl-10 pt-1"
+      placeholder="Rechercher une espèce..." v-model="search" required>
+    <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+      <svg class="w-5 h-5 text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+      </svg>
+    </div>
+
+    <ul v-if="dropdownIsOpen"
+      class="absolute top-12 left-0 bg-mine-shaft-700 rounded-3xl min-h-[30px] w-full divide-y-[1px] divide-gray-700">
+      <li v-for="(species,index) in searchedSpecies" :key="index" @click="selectOneSpecies(species)"
+        class=" pl-8 py-3 text-2xl leading-4">
+        {{ species.commonName }}</li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+  const {
+    graphql
+  } = useNhostClient()
+
+
+  // search feature
+  const search = ref('')
+  const dropdownIsOpen = ref(false)
+  const searchedSpecies = ref([])
+  const selectedSpecies = ref(null)
+
+  watch(search, (newSearch) => {
+    searchSpecies()
+    if (newSearch.length === 0 || (selectedSpecies.value !== null && newSearch === selectedSpecies.value.commonName)) {
+      dropdownIsOpen.value = false
+    } else {
+      dropdownIsOpen.value = true
+    }
+  })
+
+const emit = defineEmits(['selected-species'])
+
+  const selectOneSpecies = (species) => {
+    selectedSpecies.value = species
+    search.value = species.commonName
+    emit('selected-species', species)
+  }
+
+  const searchSpecies = async () => {
+    await useAsyncData('', async () => {
+      const res = await graphql.request(`
+      query SearchSpecies {
+        Species(limit: 10,
+        order_by: {commonName: asc},
+        where: 
+        {_or: 
+          [
+            {commonName: {_ilike: "%${search.value}%"}},
+            {scientificName: {_ilike: "%${search.value}%"}}
+          ],
+          enabled: {_eq: true}}) {
+            id
+            commonName
+        }
+      }
+       `)
+      searchedSpecies.value = res.data.Species
+    })
+  }
+
+</script>
