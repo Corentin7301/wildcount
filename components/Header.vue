87a5c69e9cc46376546d34037a69779258a04632
Corentin PERROUX <corentin7301@gmail.com>
2022-08-11 20:35:04 +0200

    add link on logo in header

--- components/Header.vue
+++ components/Header.vue
@@ -5,7 +5,7 @@
       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16"></path>
     </svg>
 
-    <img src="/logo.png" alt="wildcount logo" class=" max-w-[90px] max-h-[90px]">
+    <NuxtLink to="/"><img src="/logo.png" alt="wildcount logo" class=" max-w-[90px] max-h-[90px]"></NuxtLink>
 
     <NuxtLink to="/settings" class=" w-6 h-6">
       <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
