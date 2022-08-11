f5da10b206c0cd9d873e40f050968b1d50d797e2
Corentin PERROUX <corentin7301@gmail.com>
2022-08-11 11:27:48 +0200

    fix new observation

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,8 @@
 <template>
   <form @submit.prevent="newObservation()" class=" grid gap-9 max-w-[80%] mx-auto">
     <div class=" flex flex-col gap-5 ">
-      <SearchBar @selected-species="(species) => selectedSpecies = species" :clearedSearch="clearSearch" @cleared-search="clearSearch = false" />
+      <SearchBar @selected-species="(species) => selectedSpecies = species" :clearedSearch="clearSearch"
+        @cleared-search="clearSearch = false" />
       <input type="date" name="date" id="date"
         class="input-style p-0 pt-2 text-2xl text-center max-w-[75%] mx-auto flex items-center justify-center"
         v-model="date" required>
@@ -15,13 +16,15 @@
         class=" bg-mine-shaft-500 py-4 px-5 rounded-2xl text-white text-2xl -4 min-h-[100px] w-full resize-none -mb-2 border-none outline-none focus:border-none focus:outline-none"
         v-model="comment"></textarea>
     </div>
-    <div v-if="success" class=" text-center bg-gradient-to-t from-green-400 to-green-500 rounded-full py-1 input-shadow pb-[1px]">Observation bien répertoriée</div>
+    <div v-if="success"
+      class=" text-center bg-gradient-to-t from-green-400 to-green-500 rounded-full py-1 input-shadow pb-[1px]">
+      Observation bien répertoriée</div>
     <input type="submit" name="search" id="search" class="submit-button" value="Ajouter">
   </form>
 </template>
 
 <script setup>
-import dayjs from 'dayjs'
+  import dayjs from 'dayjs'
 
   definePageMeta({
     middleware: 'auth',
@@ -48,35 +51,44 @@ import dayjs from 'dayjs'
 
   const newObservation = async () => {
     if (selectedSpecies && comment, date, number) {
-      await useAsyncData('', async () => {
-        const res = await graphql.request(`
+      try {
+        const {
+          data,
+          error,
+          pending
+        } = await useAsyncData('new-observation', async () => {
+          await graphql.request(`
            mutation NewObservation {
              insert_Observation_one(object: 
              {
-              comment: "${comment.value}",
+               comment: "${comment.value}",
               date: "${date.value}",
               number_of_animals: ${number.value},
               user_id: "${user.value.id}",
               species_id: ${selectedSpecies.value.id}
               }) {
-           id
-         }
-       }
+                id
+            }
+          }
          `)
+        })
 
-         if (res.data.insert_Observation_one.id) {
-            newObservationOK.value = true
-            date.value = dayjs().format('YYYY-MM-DD')
-            number.value = ''
-            comment.value = ''
-            selectedSpecies.value = null
-            clearSearch.value = true
-            success.value = true
-            setTimeout(() => {
-              success.value = false
-            }, 2000)
-         }
-      })
+        if (error.value === null) {
+          newObservationOK.value = true
+          date.value = dayjs().format('YYYY-MM-DD')
+          number.value = ''
+          comment.value = ''
+          selectedSpecies.value = null
+          clearSearch.value = true
+          success.value = true
+          setTimeout(() => {
+            success.value = false
+          }, 2000)
+        }
+      } catch (e) {
+        console.log(e)
+      }
+
     } else {
       console.log('missing inputs')
     }
