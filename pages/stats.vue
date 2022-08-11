0b9f5db6f89b502028f4f46e1ed5df04ecf4aad7
Corentin PERROUX <corentin7301@gmail.com>
2022-08-11 18:01:17 +0200

    fix no refresh in list page

--- pages/stats.vue
+++ pages/stats.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    stats
+    <p class="text-xl">Je suis toujours en train de travailler sur cette page, elle ne devrait pas trop tarder à sortir ! 😉</p>
   </div>
 </template>
 
