8ac7c994d13b11179cc1a471fb3e4f752b69bf97
Corentin PERROUX <corentin7301@gmail.com>
2022-08-11 20:33:57 +0200

    fix observations graphql query

--- pages/list.vue
+++ pages/list.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
     <Container>
-
+      <p v-if="observations.data.Observation.length === 0" class=" text-center text-2xl">Il n'y a pas encore d'observation ! <NuxtLink to="/" class="block text-ecstasy-500 text-3xl">Ajoutes-en une !</NuxtLink></p>
       <!--todo:add searchbar + filters (+ update calc-max-h class in css)-->
-      <ul class=" grid grid-cols-2 gap-4 overflow-y-scroll calc-max-h no-scroll">
-        <li v-for="(observation,index) in observations.data.Observation_aggregate.nodes" :key="index"
+      <ul v-else class=" grid grid-cols-2 gap-4 overflow-y-scroll calc-max-h no-scroll">
+       <li v-for="(observation,index) in observations.data.Observation" :key="index"
           class=" min-h-[140px] rounded-2xl bg-gradient-to-b px-[1px] pt-[1px] from-ecstasy-500 via-tan-hide-500 to-transparent card-shadow">
           <div
             class="grid gap-2 bg-mine-shaft-500 pt-6 px-5 rounded-2xl text-white h-full w-full border-none outline-none">
-            <p class="text-2xl text-center leading-5 font-normal">{{observation.Species.common_name}}</p>
-            <p class="text-7xl text-center font-normal">{{observation.number_of_animals}}</p>
+            <p class="text-2xl text-center leading-5 font-normal">{{observation.Species.small_name}}</p>
+            <p class="text-7xl text-center font-normal">{{observation.Species.Observations_aggregate.aggregate.sum.number_of_animals}}</p>
           </div>
         </li>
       </ul>
@@ -32,27 +32,32 @@
     refresh()
   })
 
-// data refreshing
+  // data refreshing
   const refresh = () => refreshNuxtData('observations')
 
-// data fetching
+  // data fetching
   const {
     data: observations
   } = await useAsyncData('observations', async () => {
     const observations = await graphql.request(`
-    query nbrOfObservationsBySpecies {
-    Observation_aggregate(distinct_on: species_id, where: {user_id: {_eq: "${user.value.id}"}}) {
-      nodes {
-        number_of_animals
+    query MyQuery {
+      Observation(where: {user_id: {_eq: "${user.value.id}"}}, order_by: {species_id: asc}, distinct_on:     species_id) {
         Species {
-          common_name
+          small_name
+          Observations_aggregate {
+            aggregate {
+              sum {
+                number_of_animals
+              }
+            }
+          }
         }
       }
     }
-  }
   `)
     return observations
   })
+
 </script>
 
 <style scoped>
