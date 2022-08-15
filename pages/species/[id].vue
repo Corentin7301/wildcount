b7f263e0f6e05a73a88c29d106a4333b70f0127b
Corentin PERROUX <corentin7301@gmail.com>
2022-08-15 11:17:01 +0200

    add style of cards

--- pages/species/[id].vue
+++ pages/species/[id].vue
@@ -1,19 +1,28 @@
 <template>
   <div>
     <Container>
-        <div>
-            <h1>{{species.data.Species[0].small_name}}</h1>
-            <h2>{{species.data.Species[0].Observations_aggregate.aggregate.sum.number_of_animals}}</h2>
+      <div>
+        <div
+          class=" rounded-2xl bg-gradient-to-b px-[1px] pt-[1px] from-ecstasy-500 via-tan-hide-500 to-transparent card-shadow">
+          <div
+            class="grid gap-2 bg-mine-shaft-500 pt-7 pb-4 px-5 rounded-2xl text-white h-full w-full border-none outline-none">
+            <h1 class="text-3xl text-center leading-5 font-normal">{{species.data.Species[0].small_name}}</h1>
+            <h2 class="text-3xl text-center font-normal flex items-center justify-center gap-3">
+              Total : <span class="text-5xl">{{species.data.Species[0].Observations_aggregate.aggregate.sum.number_of_animals}}</span></h2>
+          </div>
         </div>
-        <br>
-        <br>
-        <br>
-        <div>
-            <div v-for="(observation,index) in observations.data.Observation" :key="index">
-                <p>{{observation.date}}</p>
-                <p>{{observation.number_of_animals}}</p>
-            </div>
-        </div>
+      </div>
+      <br>
+      <br>
+      <br>
+      <ul class=" grid gap-11">
+        <li v-for="(observation,index) in observations.data.Observation" :key="index"
+          class=" flex items-center justify-between min-h-[60px] rounded-b-2xl border-[1px] pl-8 pr-10 border-tan-hide-500 card-shadow">
+            <p class="text-3xl text-center leading-5 font-normal">{{observation.date}}</p>
+            <p class="text-7xl text-center font-normal mt-1">
+              {{observation.number_of_animals}}</p>
+        </li>
+      </ul>
     </Container>
   </div>
 </template>
@@ -38,7 +47,7 @@
   } = await useAsyncData('observations', async () => {
     const observations = await graphql.request(`
         query Observations {
-          Observation(where: {user_id: {_eq: "${user.value.id}"}, species_id: {_eq: ${route.params.id}}}) {
+          Observation(where: {user_id: {_eq: "${user.value.id}"}, species_id: {_eq: ${route.params.id}}}, order_by: {date: desc}) {
             id
             date
             number_of_animals
