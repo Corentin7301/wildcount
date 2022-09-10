b5626b19ab0507979ba40619ef09df90bb5f9a80
Corentin PERROUX <corentin7301@gmail.com>
2022-09-10 11:13:51 +0200

    add manifest & sw

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,44 +1,52 @@
-import { defineNuxtConfig } from 'nuxt'
+import {
+  defineNuxtConfig
+} from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    ssr: false,
-    // nitro: {
-    //   preset: 'service-worker'
-    // },
+  ssr: false,
+  // nitro: {
+  //   preset: 'service-worker'
+  // },
 
-    app: {
-        head: {
-          title: 'Nuxt 3 Awesome Starter',
-          titleTemplate: '%s - Nuxt 3 Awesome Starter',
-          meta: [
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            {
-              hid: 'description',
-              name: 'description',
-              content: 'Nuxt 3 Awesome Starter',
-            },
-          ],
-          link: [{ rel: 'icon', type: 'image/x-icon', href: '/logo.png' }],
+  app: {
+    head: {
+      title: 'Nuxt 3 Awesome Starter',
+      titleTemplate: '%s - Nuxt 3 Awesome Starter',
+      meta: [{
+          name: 'viewport',
+          content: 'width=device-width, initial-scale=1'
         },
-      },
-
-
-    modules: ['@nuxtjs/tailwindcss','nuxt-nhost','@pinia/nuxt','@nuxt/content'],
-    nhost: {
-      backendUrl: process.env.NHOST_URL,
-    },
-
-    css: [
-        // CSS file in the project
-        '@/assets/css/tailwind.css',
-        '@/assets/css/fonts.css',
-        '@/assets/css/transition.css',
-        '@/assets/css/style-perso.css',
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Nuxt 3 Awesome Starter',
+        },
       ],
-      favicon: './assets/img/icon.png',
+      link: [{
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/logo.png'
+      }],
+    },
+  },
 
-        // auto import components
-      components: true,
+
+  modules: ['@nuxtjs/tailwindcss', 'nuxt-nhost', '@pinia/nuxt', '@nuxt/content'],
+  nhost: {
+    backendUrl: process.env.NHOST_URL,
+  },
+
+  css: [
+    // CSS file in the project
+    '@/assets/css/tailwind.css',
+    '@/assets/css/fonts.css',
+    '@/assets/css/transition.css',
+    '@/assets/css/style-perso.css',
+  ],
+  favicon: './icon.png',
+
+  // auto import components
+  components: true,
 
 })
