af4a712e8c27ecfec2f38871a11f902247a0b539
Corentin PERROUX <corentin7301@gmail.com>
2022-09-12 17:07:51 +0200

    add nuxt/pwa module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,44 +1,75 @@
-import { defineNuxtConfig } from 'nuxt'
+import {
+  defineNuxtConfig
+} from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    ssr: false,
-    // nitro: {
-    //   preset: 'service-worker'
-    // },
+  ssr: false,
+  // nitro: {
+  //   preset: 'service-worker'
+  // },
 
-    app: {
-        head: {
-          title: 'Nuxt 3 Awesome Starter',
-          titleTemplate: '%s - Nuxt 3 Awesome Starter',
-          meta: [
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            {
-              hid: 'description',
-              name: 'description',
-              content: 'Nuxt 3 Awesome Starter',
-            },
-          ],
-          link: [{ rel: 'icon', type: 'image/x-icon', href: '/logo.png' }],
+  app: {
+    head: {
+      title: 'Nuxt 3 Awesome Starter',
+      titleTemplate: '%s - Nuxt 3 Awesome Starter',
+      meta: [{
+          name: 'viewport',
+          content: 'width=device-width, initial-scale=1'
         },
-      },
-
-
-    modules: ['@nuxtjs/tailwindcss','nuxt-nhost','@pinia/nuxt','@nuxt/content'],
-    nhost: {
-      backendUrl: process.env.NHOST_URL,
-    },
-
-    css: [
-        // CSS file in the project
-        '@/assets/css/tailwind.css',
-        '@/assets/css/fonts.css',
-        '@/assets/css/transition.css',
-        '@/assets/css/style-perso.css',
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Nuxt 3 Awesome Starter',
+        },
       ],
-      favicon: './assets/img/icon.png',
+      link: [{
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/logo.png'
+      }],
+    },
+  },
 
-        // auto import components
-      components: true,
+
+  modules: ['@nuxtjs/tailwindcss', 'nuxt-nhost', '@pinia/nuxt', '@nuxt/content', '@kevinmarrec/nuxt-pwa'],
+  nhost: {
+    backendUrl: process.env.NHOST_URL,
+  },
+  pwa: {
+    meta: {
+      theme_color: '#373D20',
+      name: 'WildCount',
+      author: 'Corentin PERROUX',
+      description: 'Vois ton nombre d\'observations par espèces au lieu de les calculer sur WildCount !',
+      lang: 'fr',
+    },
+    manifest: {
+      name: 'WildCount',
+      short_name: 'WildCount',
+      description: 'Vois ton nombre d\'observations par espèces au lieu de les calculer sur WildCount !',
+      lang: 'fr',
+      theme_color: "#333333",
+      background_color: "#333333",
+      display: "standalone",
+      orientation: "portrait",
+      start_url: "/?utm_source=pwa"
+    },
+    workbox: {
+      enabled: true
+    },
+  },
+
+  css: [
+    // CSS file in the project
+    '@/assets/css/tailwind.css',
+    '@/assets/css/fonts.css',
+    '@/assets/css/transition.css',
+    '@/assets/css/style-perso.css',
+  ],
+  favicon: './assets/img/icon.png',
+
+  // auto import components
+  components: true,
 
 })
