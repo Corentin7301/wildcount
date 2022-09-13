c5575a5497c6d9ea8abb9941426401c6ac0830be
Corentin PERROUX <corentin7301@gmail.com>
2022-09-13 11:21:47 +0200

    add vueUse add online checker

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,13 +2,17 @@
     <main class="relative font-Dongle font-light leading-4 text-white bg-mine-shaft-500 min-h-screen pt-10 pb-[120px] antialiased">
         <Container>
             <Header />
-            <slot />
+            <slot v-if="online" />
+            <p class=" text-3xl text-center text-red-500 underline underline-offset-4" v-else>Tu es hors ligne ! Vérifies ta connexion Internet !</p>
             <Navbar />
         </Container>
     </main>
 </template>
 
 <script setup>
+  import { useOnline } from '@vueuse/core'
+  const online = useOnline()
+
 useHead({
   title: 'Wildcount',
   // or, instead:
