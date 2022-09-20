0661d8275f13e18ea263ef13dcc37e23d5714e8a
Corentin PERROUX <corentin7301@gmail.com>
2022-09-20 14:08:02 +0200

    fix design

--- pages/list.vue
+++ pages/list.vue
@@ -2,7 +2,7 @@
   <div>
     <Container>
       <SearchBar @selected-species="(species) => selectedSpecies = species" :clearedSearch="clearSearch"
-        @cleared-search="clearSearch = false" class=" mb-4" searchbarUtility="allObservations"
+        @cleared-search="clearSearch = false" class="mb-4 " searchbarUtility="allObservations"
         @all-observations-search-value="(searchValue) => allObservationsSearch(searchValue)" />
       <!--todo: filters (+ update calc-max-h class in css)-->
       <!--
@@ -11,7 +11,7 @@
         <Filter filter="by-classes" @filter-name="(filterName) => filterChoiced = filterName">Classes</Filter>
       -->
 
-      <div class="flex gap-3 flex-nowrap overflow-x-scroll snap-x justify-between">
+      <div class="flex justify-between gap-3 overflow-x-scroll flex-nowrap snap-x">
         <div class="filter">
           <Filter @click="modalIsOpen = true">
             <template #label>
@@ -32,16 +32,16 @@
         </div>
       </div>
 
-      <p v-if="observations.data.Observation.length === 0" class=" text-center text-2xl">Il n'y a pas encore
-        d'observation ! <NuxtLink to="/" class="block text-ecstasy-500 text-3xl">Ajoutes-en une !</NuxtLink>
+      <p v-if="observations.data.Observation.length === 0" class="text-2xl text-center ">Il n'y a pas encore
+        d'observation ! <NuxtLink to="/" class="block text-3xl text-ecstasy-500">Ajoutes-en une !</NuxtLink>
       </p>
-      <ul v-else class=" grid grid-cols-2 gap-4 overflow-y-scroll snap-y scroll-pt-2 calc-max-h no-scroll rounded-2xl">
+      <ul v-else class="grid grid-cols-2 gap-4 overflow-y-scroll snap-y scroll-pt-2 calc-max-h no-scroll rounded-2xl">
         <li v-for="(observation,index) in allObservations" :key="index"
           class=" min-h-[140px] rounded-2xl bg-gradient-to-b px-[1px] pt-[1px] from-ecstasy-500 via-tan-hide-500 to-transparent snap-start card-shadow">
           <NuxtLink :to="`/species/${observation.Species.id}`"
-            class="grid gap-2 bg-mine-shaft-500 pt-6 px-5 rounded-2xl text-white h-full w-full border-none outline-none">
-            <p class="text-2xl text-center leading-5 font-normal">{{observation.Species.small_name}}</p>
-            <p class="text-7xl text-center font-normal">
+            class="grid w-full h-full gap-2 px-5 pt-6 text-white border-none outline-none bg-mine-shaft-500 rounded-2xl">
+            <p class="text-2xl font-normal leading-5 text-center">{{observation.Species.small_name}}</p>
+            <p class="font-normal text-center text-7xl">
               {{observation.Species.Observations_aggregate.aggregate.sum.number_of_animals}}</p>
           </NuxtLink>
         </li>
@@ -158,7 +158,7 @@
   .calc-max-h {
     /* base height - navbar height and filters height */
     /* max-height: calc(var(--base-max-h) - (60px + 50px)); */
-    max-height: calc(var(--base-max-h));
+    max-height: calc(var(--base-max-h) - (120px));
   }
 
 </style>
