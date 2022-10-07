3a53eacd6640b8fcfd0e87e712f06fad159db1e5
Corentin PERROUX <corentin7301@gmail.com>
2022-10-07 14:37:10 +0200

    fix pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,9 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   ssr: false,
-   nitro: {
-     preset: 'service-worker'
-   },
+  // nitro: {
+  //   preset: 'service-worker'
+  // },
 
   app: {
     head: {
