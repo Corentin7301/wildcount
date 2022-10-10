8f28871ea4e1adc7683ca0b5083b90b1f7f26879
Corentin PERROUX <corentin7301@gmail.com>
2022-10-10 12:11:59 +0200

    fix css

--- layouts/sign-layout.vue
+++ layouts/sign-layout.vue
@@ -1,10 +1,10 @@
 <template>
   <main
-    class="relative max-h-screen min-w-full min-h-screen antialiased font-light text-white grid font-Dongle leading-4 bg-mine-shaft-500">
+    class="relative grid min-w-full min-h-screen antialiased font-light leading-4 text-white font-Dongle bg-mine-shaft-500">
     <Container class="w-full ">
       <img class="w-auto h-20 mx-auto mt-16 mb-4" src="/logo.png" alt="Wildcount" />
       <slot v-if="online" />
-      <p class="text-3xl text-center text-red-500 underline  underline-offset-4" v-else>Tu es hors ligne ! Vérifies ta
+      <p class="text-3xl text-center text-red-500 underline underline-offset-4" v-else>Tu es hors ligne ! Vérifies ta
         connexion Internet !</p>
     </Container>
     <RandomImage />
